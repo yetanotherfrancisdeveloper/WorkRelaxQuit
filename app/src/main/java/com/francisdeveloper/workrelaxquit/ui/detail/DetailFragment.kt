93aed59dab698bce99c2f66bc2be9347492ea544
francesco <francesco.dicicco@ivmtech.it>
2023-09-25 13:58:59 +0200

    Fixed title setting in DetailFragment

--- app/src/main/java/com/francisdeveloper/workrelaxquit/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/francisdeveloper/workrelaxquit/ui/detail/DetailFragment.kt
@@ -5,24 +5,24 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.MobileAds
 import com.francisdeveloper.workrelaxquit.R
 import com.francisdeveloper.workrelaxquit.databinding.FragmentDetailBinding
 import com.francisdeveloper.workrelaxquit.ui.gestore.DataModel
 import com.francisdeveloper.workrelaxquit.ui.gestore.DatabaseHelper
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.Calendar
@@ -57,10 +57,10 @@ class DetailFragment : Fragment() {
         type = arguments?.getString("type").toString()
 
         // Set the header text based on the data type
-        val headerText = if (type == "Ferie") {
-            getString(R.string.header_ferie)
+        if (type == "Ferie") {
+            (activity as AppCompatActivity).supportActionBar?.title = "Ferie"
         } else {
-            getString(R.string.header_permesso)
+            (activity as AppCompatActivity).supportActionBar?.title = "Permessi"
         }
 
         val adView = binding.adView
